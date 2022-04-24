5517143435e05b5b68a6522a84c81afb489d92af
koriscs <koriscs@gmail.com>
2022-04-24 14:43:08 +0200

    Deleted unused variable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import Selections from './components/Selections';
-import PostMain from './components/PostMain';
 import './App.css';
 import {BrowserRouter as Router } from 'react-router-dom';
 
@@ -10,7 +9,6 @@ function App() {
     <Router>
       <Header />
       <Selections />
-      {/* <PostMain /> */}
     </Router>
   );
 }
